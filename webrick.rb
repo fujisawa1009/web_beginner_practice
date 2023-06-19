a5af91ef0ae1fab7bf8386d5c2eb45c4f3aba9dc
YUTA FUJISAWA <mt.fuji1009@gmail.com>
2023-06-19 19:46:47 +0900

    add#foods

--- webrick.rb
+++ webrick.rb
@@ -1,14 +1,44 @@
- # webrick.rb
+webrick.rb
+
+
+# webrick.rb
 require 'webrick'
+require "erb"
 
 server = WEBrick::HTTPServer.new({
-  :DocumentRoot => './',
-  :BindAddress => '127.0.0.1',
-  :Port => 8000
-})
+                                   :DocumentRoot => './',
+                                   :BindAddress => '127.0.0.1',
+                                   :Port => 8000
+                                 })
 
 trap(:INT){
     server.shutdown
 }
 
+WEBrick::HTTPServlet::FileHandler.add_handler("erb", WEBrick::HTTPServlet::ERBHandler)
+server.config[:MimeTypes]["erb"] = "text/html"
+
+foods = [
+  { id: 1, name: "りんご", category: "fruits" },
+  { id: 2, name: "バナナ", category: "fruits" },
+  { id: 3, name: "いちご", category: "fruits" },
+  { id: 4, name: "トマト", category: "vegetables" },
+  { id: 5, name: "キャベツ", category: "vegetables" },
+  { id: 6, name: "レタス", category: "vegetables" },
+]
+server.mount_proc("/foods") do |req, res|
+    template = ERB.new( File.read('./foods/index.erb') )
+    # ここにロジックを書く
+    @slect_params = req.query['category']
+    if @slect_params.nil?
+        @foods = foods[0..5]
+    elsif @slect_params == "all"
+        @foods = foods[0..5]
+    elsif @slect_params == "fruits"
+        @foods = foods[0..2]
+    elsif @slect_params == "vegetables"
+        @foods = foods[3..5]
+    end
+    res.body << template.result( binding )
+end
 server.start
